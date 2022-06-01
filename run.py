83e479fce134594c2de2b22af422c4f56d2b9524
Erin Doherty <erin.doherty@team.telstra.com>
2022-06-01 15:22:28 +1000

    fix

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import geopandas as gp
 
-df = gp.read_file('Speed_Zones.shp').to_crs(epsg=4326)
-df = df.loc[df['SIGN_SPEED'] == 40]
+df = gp.read_file('Speed_Zones.shp')
+df = df.loc[df["Variable_C"].str.contains("SCHOOL", na=False)]
+df = df.to_crs(epsg=4326)
 with open('Speed_Zones.json', 'w') as f:
     f.write(df.to_json())
